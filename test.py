c7b2f60a8116041825557f5669cfd2a033468aea
hassan2112d <hassan2112d@aptechgdn.net>
2023-11-12 18:29:05 +0500

    hello

--- test.py
+++ test.py
@@ -13,7 +13,11 @@ soup = BeautifulSoup(content,'html.parser')
 
 anchor = soup.find_all('a')
 
-for link in anchor:
-    if(link.get('href') != '#'):
-        linkadd = link.get('href')
-        print(linkadd)
+with open("text.txt","a") as a:
+    for i in anchor:
+        link = i.get('href')
+
+        if link is not None and link != '#':
+            print(link)
+            
+            a.write(link + '/n')
